dae607516f48fefd712ff7822f09181881b5d8b4
Kalyan Kumar Thanguturi <81627943+kalyan114@users.noreply.github.com>
2022-12-28 05:44:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { component } from 'react';
+import logo from './logo.svg';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './EventBind';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      {/* <Greet /> */}
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      <EventBind />
+    </div>
+  );
+}
+
+export default App;
